bb671352d3999d6404f6183d6b9e415e6d02a90b
owen151128 <owen151128@gmail.com>
2017-07-11 18:57:49 +0900

    tmp

---
+++ app/src/main/java/com/hpcnt/mvptwithvolley/contract/MainContract.java
@@ -0,0 +1,8 @@
+package com.hpcnt.mvptwithvolley.contract;
+
+/**
+ * Created by 0wen151128 on 2017. 7. 11..
+ */
+
+public interface MainContract {
+}
---
+++ app/src/main/java/com/hpcnt/mvptwithvolley/presenter/MainPresenter.java
@@ -0,0 +1,8 @@
+package com.hpcnt.mvptwithvolley.presenter;
+
+/**
+ * Created by 0wen151128 on 2017. 7. 11..
+ */
+
+public class MainPresenter {
+}
